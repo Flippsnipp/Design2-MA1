cc2a3a46cf6662eaacf35b3fd493fc1f8da078b7
Filip <filip1982nordhagen@gmail.com>
2020-10-16 14:35:35 +0200

    commiting final draft

--- script/script.js
+++ script/script.js
@@ -1,8 +1,6 @@
 document.querySelectorAll(`[id^= "terminal_"]`).forEach(function(terminalElem){
     console.log(terminalElem.id, terminalElem);
 
-
-
     //create popup ID based on terminal ID 
     const popupId = ("popup_" + terminalElem.id.split("_")[1]); 
 
@@ -19,4 +17,61 @@ document.querySelectorAll(`[id^= "terminal_"]`).forEach(function(terminalElem){
         popupElem.classList.remove("popup-open");
     }) 
 
+});
+
+document.querySelectorAll(`[id^= "stop_"]`).forEach(function(stopElem){
+    console.log(stopElem.id, stopElem);
+
+    //create stop ID based on Line ID
+    const lineID = "line_" + stopElem.id.split("_")[1];
+    
+
+    // targeting line by ID
+    const lineElem = document.getElementById(lineID);
+
+    // set up hover and unhover event for stop
+    stopElem.addEventListener("mouseover", function (event) {
+        lineElem.classList.add("lineNumber");
+    })
+
+    stopElem.addEventListener("mouseout", function (event) {
+        lineElem.classList.remove("lineNumber");
+    }) 
+    
+});
+
+document.querySelectorAll(`[id^= "line_"]`).forEach(function(lineElem){
+    console.log(lineElem.id, lineElem);
+
+    //create line ID based on stop ID
+    const stopID = "stop_" + lineElem.id.split("_")[1];
+    
+
+    // targeting stop by ID
+    const stopElem = document.getElementById(stopID);
+
+    // set up hover and unhover event for line
+    lineElem.addEventListener("mouseover", function (event) {
+        stopElem.classList.add("numberLine");
+    })
+
+    lineElem.addEventListener("mouseout", function (event) {
+        stopElem.classList.remove("numberLine");
+    }) 
+    
+});
+
+
+document.querySelectorAll(`[id^= "line_"]`).forEach(function(line) {
+    line.addEventListener("mouseover", function (event) {
+        // move this line to the front
+        event.currentTarget.parentElement.append(event.currentTarget);
+    });
+});
+
+document.querySelectorAll(`[id^= "stop_"]`).forEach(function(stop) {
+    stop.addEventListener("mouseover", function (event) {
+        // move this stop to the front
+        event.currentTarget.parentElement.append(event.currentTarget);
+    });
 });
